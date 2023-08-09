8e8fb481426ae79b57badc9b3bb10b302eb23376
pxxnxx <kmin9811@naver.com>
2023-08-09 13:34:55 +0900

    :twisted_rightwards_arrows: Openvidu, DashBoard 브랜치 합병

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,7 +3,7 @@ import Conference from "./pages/Conference";
 import Dash from "./pages/Dash";
 import Login from "./pages/Login";
 import Landing from "./pages/Landing";
-// import OpenVidu from "./openvidu/App";
+import OpenVidu from "./openvidu/App";
 import Group from "./components/Group";
 import Calendar from "./components/Calendar";
 import Meeting from "./components/Meeting";
@@ -26,7 +26,6 @@ import JoinMeeting from "./components/JoinMeeting";
 import Gcreate from "./components/group/Gcreate";
 import Month from "./components/plan/Month";
 import Welcome from "./components/group/Welcome";
-import Waiting from "./pages/Waiting";
 function App() {
   return (
     <>
@@ -35,8 +34,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />}></Route>
           <Route path="/dash" element={<Dash />}>
-            {/* <Route path="" element={<Navigate to="group" />}></Route> */}
-            <Route path="group" element={<Group />}>
+            <Route path="" element={<Navigate to="group" />}></Route>
+            <Route path="group">
               <Route path="none" element={<Welcome />}></Route>
               {/* <Route path="" element={<GChat />}></Route> */}
               <Route path="create" element={<Gcreate />}></Route>
@@ -56,7 +55,7 @@ function App() {
               <Route path="start" element={<Start />}></Route>
               <Route path="manage" element={<Manage />}></Route>{" "}
               <Route path="joinmeeting" element={<JoinMeeting />}></Route>
-              <Route path="wait" element={<Waiting />}></Route>
+              {/*<Route path="wait" element={<Waiting />}></Route>*/}
               <Route
                 path="conference"
                 element={<Conference sessionId="" myUserName="" />}
@@ -68,6 +67,12 @@ function App() {
               <Route path="draw" element={<Draw />}></Route>
             </Route>
           </Route>
+          <Route path="join" element={<JoinMeeting />}></Route>
+          <Route path="/wait" element={<OpenVidu />} />
+          <Route
+            path="conference"
+            element={<Conference sessionId="" myUserName="" />}
+          ></Route>
           <Route path="/info" element={<Info />}></Route>
           <Route path="/login" element={<Login />}></Route>
         </Routes>
