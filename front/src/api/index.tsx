5694dcd67a795e31e719d213708bae0803e3b998
김기정 <cjdfidrlwjd@naver.com>
2023-08-09 13:28:38 +0900

    :recycle: Login.tsx 소셜 로그인 로직 및 Axios apiInstance 수정 기존에 axios로 서버에 소셜 로그인 요청을 보내던 로직을 태그로 수정 Axios apiInstance에 jwt 토큰 헤더 추가 Resolves #17

--- front/src/api/index.tsx
+++ front/src/api/index.tsx
@@ -1,11 +1,13 @@
-import axios, {Axios} from "axios";
+import axios from "axios";
 
 // local vue api axios instance
 function apiInstance() {
-    const instance : Axios = axios.create({
+    const accessToken  = localStorage.getItem("accessToken");
+    const instance  = axios.create({
         baseURL: "http://localhost:8080/",
         headers: {
             "Content-Type": "application/json;charset=utf-8",
+            "Authorization": `Bearer ${accessToken}`,
         },
     });
     return instance;
--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -4,20 +4,8 @@ import loginN from "../assets/btnW_완성형.png";
 import loginK from "../assets/kakao_login_medium_narrow.png";
 import fillerImg from "../assets/bwink_edu_04_single_04.jpg";
 import RoundCard from "../components/atoms/RoundCard";
-import {apiInstance} from "../api";
 
-const api = apiInstance();
 function Login() {
-  // const headerHeight = 78; // Change this value to match your actual header height
-  // const colHeight = `calc(100vh - ${headerHeight}px)`;
-  const handleLoginClick = (e: React.MouseEvent<HTMLImageElement, MouseEvent>, type : string) => {
-    console.log(e);
-    api.post(`oauth2/authorization/${type}`).then(res=>{
-      console.log('res : ' + res)
-    }).catch(err => {
-      console.log('err : ' + err)
-    })
-  };
   return (
     <>
       <RoundCard height="300px" width="600px">
@@ -35,15 +23,15 @@ function Login() {
           </Col>
 
           <Col className="position-relative py-2 px-4 d-flex flex-column align-items-center justify-content-center">
-            <div className="mb-4">
-              <img src={loginN} alt="naver login " width={183} onClick={e=>handleLoginClick(e, "naver")}/>
-            </div>
-            <div className="mb-4">
-              <img src={loginK} alt="kakao login" onClick={e=>handleLoginClick(e, "kakao")}/>
-            </div>
-            <div className="mb-4">
-              <img src={loginG} alt="google login" width={184} onClick={e=>handleLoginClick(e, "google")}/>
-            </div>
+            <a className="mb-4" href={import.meta.env.VITE_NAVER_AUTH_URL}>
+              <img src={loginN} alt="naver login " width={183}/>
+            </a>
+            <a className="mb-4" href={import.meta.env.VITE_KAKAO_AUTH_URL}>
+              <img src={loginK} alt="kakao login" />
+            </a>
+            <a className="mb-4" href={import.meta.env.VITE_GOOGLE_AUTH_URL}>
+              <img src={loginG} alt="google login" width={184}/>
+            </a>
           </Col>
         </Row>
       </RoundCard>
