e8a8c176bd67511e9f0635b16daa1cb3ca9b5d78
김기정 <cjdfidrlwjd@naver.com>
2023-08-17 09:02:40 +0900

    :zap: redux persist 적용

--- front/src/store/store.ts
+++ front/src/store/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "../store/user";
 import groupReducer from "../store/group";
 import planReducer from "../store/plan";
@@ -6,23 +6,27 @@ import meetingReducer from "../store/meeting";
 import itemReducer from "../store/item";
 import statusReducer from "../store/test";
 import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 
 const persistConfig = {
   key: "root", // localStorage key
   storage, // localStorage
-  whitelist: ["auth"], // target (reducer name)
-}
+};
 
-
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    groups: groupReducer,
-    plan: planReducer,
-    meeting: meetingReducer,
-    status: statusReducer,
-    item: itemReducer,
-  },
+const rootReducer = combineReducers({
+  user: userReducer,
+  groups: groupReducer,
+  plan: planReducer,
+  meeting: meetingReducer,
+  status: statusReducer,
+  item: itemReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const store = configureStore({ reducer: persistedReducer });
+export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export default store;
